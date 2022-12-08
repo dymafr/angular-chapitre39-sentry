04bffab1c7bcc2749c5479411245ebaaf57dadcc
dymafr <contact@dyma.fr>
2022-12-08 10:53:37 +0100

    maj angular 15

--- src/app/errors.ts
+++ src/app/errors.ts
@@ -1,19 +1,18 @@
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Injectable, isDevMode } from '@angular/core';
 import * as Sentry from '@sentry/browser';
-import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class MyErrorHandler implements ErrorHandler {
   constructor() {
-    if (environment.production) {
+    if (!isDevMode()) {
       Sentry.init({
-        dsn: 'https://54d5c9467d1448b2878c5071e547e1b6@sentry.io/1770322'
+        dsn: 'https://54d5c9467d1448b2878c5071e547e1b6@sentry.io/1770322',
       });
     }
   }
 
   handleError(error: any) {
-    if (environment.production) {
+    if (!isDevMode()) {
       Sentry.captureException(error.originalError || error);
     }
     console.error(error);
